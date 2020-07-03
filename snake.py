6f01db61eb2f354316b967e04ce329d24f62f191
Darshil Papalkar <67659174+Darshil-Papalkar@users.noreply.github.com>
2020-07-03 20:55:53 +0530

    Update snake.py

--- snake.py
+++ snake.py
@@ -1,3 +1,4 @@
+#Use alt+esc to exit the game
 import pygame
 import time
 import random 
@@ -97,7 +98,16 @@ def gameLoop():
                     x1_change = 0
 
         if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0:
-            game_close = True
+            if x1 >= dis_width:
+                x1 = 0
+            elif x1 < 0:
+                x1 = dis_width
+            elif y1 >= dis_height:
+                y1 = 0
+            elif y1 < 0:
+                y1 = dis_height
+
+            #game_close = True
         x1 += x1_change
         y1 += y1_change
         dis.fill(blue)
@@ -109,9 +119,16 @@ def gameLoop():
         if len(snake_List) > Length_of_snake:
             del snake_List[0]
 
-        for x in snake_List[:-1]:
-            if x == snake_Head:
-                game_close = True
+        #for x in snake_List[:-1]:
+            #if x == snake_Head:
+            #    game_close = True
+        for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_q:
+                        game_close = True
+                    if event.key == pygame.K_c:
+                        gameLoop()
+        
 
         our_snake(snake_block, snake_List)
         Your_score(Length_of_snake - 1)
